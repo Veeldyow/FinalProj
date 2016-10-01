fc57816f4763b6e1616eb9eb0f2719fc40986ab2
Veeldyow <veeldyow@yahoo.com>
2016-10-02 04:40:52 +0800

    Final

--- app/src/main/java/viljo/finalproj/SplashScreen.java
+++ app/src/main/java/viljo/finalproj/SplashScreen.java
@@ -10,7 +10,7 @@ public class SplashScreen extends Activity{
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(4000);
+                    sleep(3000);
                 }
                 catch(InterruptedException e){
                     e.printStackTrace();
